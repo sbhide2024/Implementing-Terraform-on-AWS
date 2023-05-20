91765147d72e6d8020ecff617060a2eb72ec993e
sbhide2024 <84491831+sbhide2024@users.noreply.github.com>
2023-05-20 12:27:18 +0530

    Update main.tf

--- 1-dev-vpc/main.tf
+++ 1-dev-vpc/main.tf
@@ -1,98 +1,19 @@
 
-#################################################################################
-# PROVIDERS
-##################################################################################
-
-
-
-
-#############################################################################
-# VARIABLES
-#############################################################################
-
-variable "region" {
-  type    = string
-  default = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
 
-
-variable "vpc_cidr_range" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
-variable "public_subnets" {
-  type    = list(string)
-  default = ["10.0.0.0/24", "10.0.1.0/24"]
-}
-
-variable "database_subnets" {
-  type    = list(string)
-  default = ["10.0.8.0/24", "10.0.9.0/24"]
-}
-
-
-#############################################################################
-# PROVIDERS
-#############################################################################
-
+# Configure the AWS Provider
 provider "aws" {
-  region = var.region
-  AWS_ACCESS_KEY_ID=var.AWS_ACCESS_KEY_ID
-  AWS_SECRET_ACCESS_KEY=var.AWS_SECRET_ACCESS_KEY
-   
-  # access_key = var.AWS_ACCESS_KEY
- # secret_key = var.AWS_SECRET_KEY
-  
-  }
-
-#############################################################################
-# DATA SOURCES
-#############################################################################
-
-data "aws_availability_zones" "azs" {}
-
-#############################################################################
-# RESOURCES
-#############################################################################  
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
-
-  name = "dev-vpc"
-  cidr = var.vpc_cidr_range
-
-  azs            = slice(data.aws_availability_zones.azs.names, 0, 2)
-  public_subnets = var.public_subnets
-
-  # Database subnets
-  database_subnets = var.database_subnets
-  database_subnet_group_tags = {
-    subnet_type = "database"
-  }
-
-  tags = {
-    Environment = "dev"
-    Team        = "infra"
-  }
-
+  region = "us-east-1"
 }
 
-#############################################################################
-# OUTPUTS
-#############################################################################
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
-
-output "db_subnet_group" {
-  value = module.vpc.database_subnet_group
-}
-
-output "public_subnets" {
-  value = module.vpc.public_subnets
-}
-
-
