eb0c246bcf88db6d5e33358c0261a680d6b9a2b2
sbhide2024 <84491831+sbhide2024@users.noreply.github.com>
2023-05-20 14:51:25 +0530

    Update main.tf

--- 1-dev-vpc/main.tf
+++ 1-dev-vpc/main.tf
@@ -5,8 +5,8 @@
 provider "aws" {
 #  access_key = "ACCESS_KEY"
 #  secret_key = "SECRET_KEY"
-  AWS_ACCESS_KEY_ID=var.AWS_ACCESS_KEY_ID
-  AWS_SECRET_ACCESS_KEY=var.AWS_SECRET_ACCESS_KEY
+  AWS_ACCESS_KEY_ID="${var.AWS_ACCESS_KEY_ID}"
+  AWS_SECRET_ACCESS_KEY="${var.AWS_SECRET_ACCESS_KEY}"
   
   region     = "us-east-1"
 }
