668697be7aa58e30b46fb6293ce6742a29eca7ec
sbhide2024 <84491831+sbhide2024@users.noreply.github.com>
2023-05-20 16:22:54 +0530

    Update main.tf

--- 1-dev-vpc/main.tf
+++ 1-dev-vpc/main.tf
@@ -2,7 +2,7 @@
 # PROVIDERS #
 ##################################################################################
 terraform {
-  required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_version = "~> 2.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,6 +10,8 @@ terraform {
     }
   }
 }
+
+
 
 provider "aws" {
   region  = us-east-1
