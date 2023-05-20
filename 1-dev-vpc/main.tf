a6404af62123c998d5cfc493cdd771f81fb494ee
sbhide2024 <84491831+sbhide2024@users.noreply.github.com>
2023-05-20 11:33:27 +0530

    Update main.tf

--- 1-dev-vpc/main.tf
+++ 1-dev-vpc/main.tf
@@ -3,11 +3,7 @@
 # PROVIDERS
 ##################################################################################
 
-provider "aws" {
-  access_key = "ACCESS_KEY"
-  secret_key = "SECRET_KEY"
-  region     = "us-east-1"
-}
+
 
 
 #############################################################################
@@ -42,7 +38,9 @@ variable "database_subnets" {
 
 provider "aws" {
   region = var.region
-}
+   access_key = "ACCESS_KEY"
+  secret_key = "SECRET_KEY"
+  }
 
 #############################################################################
 # DATA SOURCES
